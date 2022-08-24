e842f4c87e408aaad1786536f5f905f6c9128b55
kingsae <kingsae1@naver.com>
2022-08-24 13:46:21 +0900

    Update

--- index.js
+++ index.js
@@ -1,77 +1,81 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 class RTObserver {
   constructor(config) {
-    this.config = config;
+    this.config = config
   }
 }
 
 class RTMongoObserver extends RTObserver {
-  db = undefined;
-  collection = undefined;
-  client = undefined;
-  target = {};
+  db = undefined
+  collection = undefined
+  client = undefined
+  target = {}
 
   constructor(config) {
-    super(config);
+    super(config)
 
-    if (this.type === "MONGODB") {
-      mongoose
-        .connect(
-          `mongodb+srv://${this.config.SERVER_ID}:${this.config.SERVER_PW}@${this.config.SERVER_URL}`,
-          { dbName: this.config.MONGO_DATABASE }
-        )
-        .then(async () => {
-          console.log("[Start] MongoDB ! ");
-          this.server = mongoose.connection;
-        });
-    }
+    mongoose
+      .connect(
+        `mongodb+srv://${this.config.SERVER_ID}:${this.config.SERVER_PW}@${this.config.SERVER_URL}`,
+        { dbName: this.config.MONGO_DATABASE },
+      )
+      .then(async () => {
+        console.log('[Start] MongoDB ! ')
+        this.server = mongoose.connection
+      })
+      .catch((e) => {
+        console.log('[Error] MongoDB !!!', e)
+      })
   }
 
-  getCollection = (collection = this.config.MONGO_COLLECTION) => {
+  getCollection = (
+    collection = this.config.MONGO_COLLECTION,
+    inSchema = {},
+  ) => {
     if (!this.collection) {
-      const schema = new mongoose.Schema({});
-      this.collection = mongoose.model(collection, schema);
+      const schema = new mongoose.Schema(inSchema)
+      this.collection = mongoose.model(collection, schema)
     }
 
-    return this.collection;
-  };
+    return this.collection
+  }
 
   subscribe = (target, option) => {
     if (!target) {
       console.log(
-        "[RTMongoObserver] Error : Must input first parameter (db or collection)"
-      );
-      return;
+        '[RTMongoObserver] Error : Must input first parameter (db or collection)',
+      )
+      return
     }
 
-    target.uuid = new Date().getTime();
+    target.uuid = new Date().getTime()
 
-    console.log("[RTMongoObserver] subscribe - uuid : " + target.uuid);
+    console.log('[RTMongoObserver] subscribe - uuid : ' + target.uuid)
 
     return new Promise((resolve, reject) => {
-      const changeStream = target.watch(option);
+      const changeStream = target.watch(option)
       this.target = {
         [target.uuid]: changeStream,
-      };
+      }
 
-      changeStream.on("change", (data) => {
-        resolve(data);
-      });
-    });
-  };
+      changeStream.on('change', (data) => {
+        resolve(data)
+      })
+    })
+  }
 
   unsubscribe = (target) => {
     return new Promise((resolve, reject) => {
       if (target && target.uuid) {
-        console.log("[RTMongoObserver] unsubscribe - uuid : " + target.uuid);
-        this.target[target.uuid].close();
-        resolve(true);
+        console.log('[RTMongoObserver] unsubscribe - uuid : ' + target.uuid)
+        this.target[target.uuid].close()
+        resolve(true)
       }
-      reject(false);
-    });
-  };
+      reject(false)
+    })
+  }
 }
 
-module.exports.RTObserver = RTObserver;
-module.exports.RTMongoObserver = RTMongoObserver;
+module.exports.RTObserver = RTObserver
+module.exports.RTMongoObserver = RTMongoObserver
